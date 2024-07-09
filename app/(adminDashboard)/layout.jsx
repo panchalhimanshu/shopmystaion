24d95717d0ca00dd3127d40adca5a79614c3c980
panchal  himanshu <panchalhimanshu660@gmail.com>
2024-07-09 12:08:20 +0530

    .

--- app/(adminDashboard)/layout.jsx
+++ app/(adminDashboard)/layout.jsx
@@ -5,29 +5,29 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const Layout = ({ children }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-     const handleRouteChange = () => {
-       window.scrollTo(0, 0);
-     };
-    const fetchData = async () => {
-      const logindata = sessionStorage.getItem('userData');
-      if (logindata) {
-        const userData = JSON.parse(logindata);
-        if (userData.roleid !== 1) {
-          router.push('/');
-          setTimeout(() => { toast("Not authorized") }, 10);
-        }
-      }else{
-        router.push('/');
-      }
+  // useEffect(() => {
+  //    const handleRouteChange = () => {
+  //      window.scrollTo(0, 0);
+  //    };
+  //   const fetchData = async () => {
+  //     const logindata = sessionStorage.getItem('userData');
+  //     if (logindata) {
+  //       const userData = JSON.parse(logindata);
+  //       if (userData.roleid !== 1) {
+  //         router.push('/');
+  //         setTimeout(() => { toast("Not authorized") }, 10);
+  //       }
+  //     }else{
+  //       router.push('/');
+  //     }
       
-    };
+  //   };
 
-    fetchData();
-    handleRouteChange();
-  }, [router]);
+  //   fetchData();
+  //   handleRouteChange();
+  // }, [router]);
 
   return <MainLayout>{children}</MainLayout>;
 };
--- app/(stationDashboard)/layout.jsx
+++ app/(stationDashboard)/layout.jsx
@@ -5,24 +5,24 @@ import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 const Layout = ({ children }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const logindata = sessionStorage.getItem('userData');
-      if (logindata) {
-        const userData = JSON.parse(logindata);
-        if (userData.roleid !== 5) {
-          router.push('/');
-          setTimeout(() => { toast("Not authorized") }, 10);
-        }
-      }else{
-        router.push('/');
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const logindata = sessionStorage.getItem('userData');
+  //     if (logindata) {
+  //       const userData = JSON.parse(logindata);
+  //       if (userData.roleid !== 5) {
+  //         router.push('/');
+  //         setTimeout(() => { toast("Not authorized") }, 10);
+  //       }
+  //     }else{
+  //       router.push('/');
+  //     }
+  //   };
 
-    fetchData();
-  }, [router]);
+  //   fetchData();
+  // }, [router]);
 
   return <MainLayout>{children}</MainLayout>;
 };
